a361f654f2b68b94d19d27fa4988f23b9d5202de
jankey <vasili.coder@gmail.com>
2023-03-31 18:07:56 +0300

    configure open and close method

--- storage/storage.go
+++ storage/storage.go
@@ -1,8 +1,12 @@
 package storage
 
+import "database/sql"
+
 // Storage * Instance of storage
 type Storage struct {
 	config *Config
+	// * Database FileDescriptor
+	db *sql.DB
 }
 
 // New * Storage Constructor
@@ -14,10 +18,18 @@ func New(config *Config) *Storage {
 
 // Open * Open connection method
 func (storage *Storage) Open() error {
+	db, err := sql.Open("postgres", storage.config.DatabaseURI)
+	if err != nil {
+		return err
+	}
+	if err := db.Ping(); err != nil {
+		return err
+	}
+	storage.db = db
 	return nil
 }
 
 // Close * Close connection
 func (storage *Storage) Close() {
-
+	storage.db.Close()
 }
