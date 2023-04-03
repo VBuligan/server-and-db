f15c8a4f76036b162f2e0e7fdcd2c722b4f25c66
jankey <vasili.coder@gmail.com>
2023-04-03 13:40:00 +0300

    API Handler methods 1

--- internal/app/api/hendlers.go
+++ internal/app/api/hendlers.go
@@ -1,6 +1,9 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 // * Full API Handler initialization file
 
@@ -14,3 +17,32 @@ type Message struct {
 func initHeaders(writer http.ResponseWriter) {
 	writer.Header().Set("Content-Type", "application/json")
 }
+
+// GetAllArticles * Возвращает все статьи из дб
+func (api *API) GetAllArticles(writer http.ResponseWriter, req *http.Request) {
+	// * Инициализация хедеров
+	initHeaders(writer)
+	// * Логируем момент начала обработки запроса
+	api.logger.Info("Get All Articles GET /api/v1/articles")
+	// * Пытаемся получить что то от бд
+	articles, err := api.storage.Article().SelectAll()
+	if err != nil {
+		// * Что делаем если была ошибка на этапе подключения
+		api.logger.Info("Error while Articles.SelectAll :", err)
+		msg := Message{
+			StatusCode: 501,
+			Message:    "We have some troubles to accessing, try again later",
+			IsError:    true,
+		}
+		writer.WriteHeader(501)
+		json.NewEncoder(writer).Encode(msg)
+		return
+	}
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode(articles)
+}
+
+func (api *API) GetArticleById(writer http.ResponseWriter, req *http.Request)    {}
+func (api *API) DeleteArticleById(writer http.ResponseWriter, req *http.Request) {}
+func (api *API) PostArticle(writer http.ResponseWriter, req *http.Request)       {}
+func (api *API) PostUserRegister(writer http.ResponseWriter, req *http.Request)  {}
