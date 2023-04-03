031330e07078ac58b09631a2cd2fce4e951e7dde
jankey <vasili.coder@gmail.com>
2023-04-03 13:27:10 +0300

    API Handler initmfunc

--- internal/app/api/hendlers.go
+++ internal/app/api/hendlers.go
@@ -1,3 +1,16 @@
 package api
 
+import "net/http"
+
 // * Full API Handler initialization file
+
+// Message * Вспомогательная структура для сообщений
+type Message struct {
+	StatusCode int    `json:"statusCode"`
+	Message    string `json:"message"`
+	IsError    bool   `json:"is_error"`
+}
+
+func initHeaders(writer http.ResponseWriter) {
+	writer.Header().Set("Content-Type", "application/json")
+}
